17387aefcb7f89b1d7acf60d7f8f470896fc3368
guzeradimazz <Guzerashop@mail.ru>
2022-05-24 22:25:04 +0300

    MYSQL connected successfully + docker

--- instagram_django/instagram_django/instagram_django/settings.py
+++ instagram_django/instagram_django/instagram_django/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'instagram_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'INSTAGRAM',
+        'USER': 'root',
+        'PASSWORD': 'jblguzerajbL1',
+        'HOST':'localhost',
+        'PORT': '3306',
     }
 }
 
